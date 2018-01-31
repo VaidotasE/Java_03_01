8fa488fd61f945962a3d04fc8e99da7cea498cbc
Vaidotas <vaidotas.eigelis@gmail.com>
2018-01-31 10:40:34 +0200

    3day code 01

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,6 +13,15 @@ public class Main {
          * ir po to parašytų ar šis skaičius yra lyginis ar ne lyginis.
          * Užuomina panaudokite % operatoriu.
          * */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ivesk skaiciu: ");
+        int a = sc.nextInt();
         LyginisNelyginis objektas = new LyginisNelyginis();
+
+        objektas.setA(a);
+
+        System.out.println("Ivesta skaicius: "+objektas.getA());
+        objektas.skaiciuok();
+
     }
 }
